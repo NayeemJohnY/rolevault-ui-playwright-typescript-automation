081989c1f5b85b467e3cb606757fd48f97a00993
NayeemJohnY <njnayeem@gmail.com>
2025-09-09 23:11:46 +0530

    fix: standardize run-name format in Playwright test execution workflow feat(tests): add login test case to homepage.spec.ts

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -5,3 +5,15 @@ test('Launch RoleVault and Verify Title', async ({ page }) => {
   await expect(page).toHaveTitle('RoleVault')
   await expect(page.getByRole('heading', { name: 'Role Vault' })).toBeVisible()
 });
+
+
+test('Should user able to Login', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveTitle('RoleVault')
+  await expect(page.getByRole('heading', { name: 'Role Vault' })).toBeVisible()
+  await page.getByLabel('Email Address').fill('admin@test.com');
+  await page.getByLabel('Password').fill('admin123');
+  await page.getByTestId('login-submit').click();
+  await expect(page.getByRole('heading', { name: 'Dashboard', level: 1 })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Profile', exact: true })).toBeVisible();
+});
