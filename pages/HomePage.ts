62128b3cad47ccb3af7faec08f882b9c563113d7
NayeemJohnY <njnayeem@gmail.com>
2025-09-12 23:26:07 +0530

    feat: Enhance user authentication tests with improved error handling and structure feat: Enhance HomePage class with registration functionality and additional locators

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -6,18 +6,36 @@ export class HomePage {
     readonly $emailAddress: Locator;
     readonly $password: Locator;
     readonly $login: Locator;
+    readonly $registerTab: Locator;
+    readonly $fullName: Locator;
+    readonly $confirmPassword: Locator;
+    readonly $createAccount: Locator
+
 
     constructor(page: Page) {
         this.page = page;
         this.$emailAddress = page.getByLabel('Email Address');
-        this.$password = page.getByLabel('Password')
-        this.$login = page.getByTestId('login-submit')
+        this.$password = page.getByLabel('Password', { exact: true });
+        this.$login = page.getByTestId('login-submit');
+        this.$registerTab = page.getByTestId('register-tab');
+        this.$fullName = page.getByLabel('Full Name');
+        this.$confirmPassword = page.getByRole('textbox', { name: 'Confirm Password' });
+        this.$createAccount = page.getByTestId('register-submit');
     }
 
     public async login(user): Promise<void> {
         await this.$emailAddress.fill(user.emailAddress);
         await this.$password.fill(user.password);
         await this.$login.click();
+    }
+
+    public async register(registerUser): Promise<void> {
+        await this.$registerTab.click();
+        await this.$fullName.fill(registerUser.fullName)
+        await this.$emailAddress.fill(registerUser.emailAddress);
+        await this.$password.fill(registerUser.password);
+        await this.$confirmPassword.fill(registerUser.password);
+        await this.$createAccount.click();
     }
 
 }
