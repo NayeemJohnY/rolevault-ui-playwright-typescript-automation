08d3013d115d6b327621b99a656fb3b84ec9c5ff
NayeemJohnY <njnayeem@gmail.com>
2025-09-24 13:27:55 +0530

    feat: Implement App class to manage page instances and common assertions for improved structure

---
+++ pages/App.ts
@@ -0,0 +1,37 @@
+import { Page } from '@playwright/test';
+import { CommonAssertions } from './common/assertions';
+import { commonLocators } from './common/locators';
+import { DashboardPage } from './DashboardPage';
+import { HomePage } from './HomePage';
+import { UsersPage } from './UsersPage';
+
+
+export class App {
+    constructor(public readonly page: Page) { }
+
+    private _homePage?: HomePage;
+    private _dashboardPage?: DashboardPage;
+    private _usersPage?: UsersPage;
+    private _assert?: CommonAssertions;
+    private _ui?: ReturnType<typeof commonLocators>;
+
+    get homePage() {
+        return this._homePage ??= new HomePage(this.page);
+    }
+
+    get dashboardPage() {
+        return this._dashboardPage ??= new DashboardPage(this.page);
+    }
+
+    get usersPage() {
+        return this._usersPage ??= new UsersPage(this.page);
+    }
+
+    get assert() {
+        return this._assert ??= new CommonAssertions(this.page);
+    }
+
+    get ui() {
+        return this._ui ??= commonLocators(this.page);
+    }
+}
