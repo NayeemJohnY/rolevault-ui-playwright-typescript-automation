e6c3964f9c0adf6b0f8ef799a687ea510f4e3c48
NayeemJohnY <njnayeem@gmail.com>
2025-09-30 20:15:07 +0530

    feat: Enhance test user and application launch functionality with detailed JSDoc comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,8 @@ export default defineConfig([
             "playwright-report",
             "test-results",
             "rolevault-webapp",
+            'allure-report',
+            'allure-results'
         ]
     },
     {
